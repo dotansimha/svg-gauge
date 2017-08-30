cce778b5a6e12806d014fb282bb8eb2df2db9f30
dotansimha <dotansimha@gmail.com>
2017-08-30 08:59:01 +0300

    change color

--- src/gauge.js
+++ src/gauge.js
@@ -1,27 +1,29 @@
 /* global window, define, module */
-(function(global, factory) {
+(function (global, factory) {
   var Gauge = factory(global);
-  if(typeof define === "function" && define.amd) {
+  if (typeof define === "function" && define.amd) {
     // AMD support
-    define(function() {return Gauge;});
-  }else if(typeof module === "object" && module.exports) {
+    define(function () {
+      return Gauge;
+    });
+  } else if (typeof module === "object" && module.exports) {
     // CommonJS support
     module.exports = Gauge;
-  }else {
+  } else {
     // We are probably running in the browser
     global.Gauge = Gauge;
   }
-})(typeof window === "undefined" ? this : window, function(global, undefined) {
+})(typeof window === "undefined" ? this : window, function (global, undefined) {
 
   var document = global.document,
     slice = Array.prototype.slice,
     requestAnimationFrame = (global.requestAnimationFrame ||
-        global.mozRequestAnimationFrame ||
-        global.webkitRequestAnimationFrame ||
-        global.msRequestAnimationFrame ||
-        function(cb) {
-          return setTimeout(cb, 1000 / 60);
-        });
+      global.mozRequestAnimationFrame ||
+      global.webkitRequestAnimationFrame ||
+      global.msRequestAnimationFrame ||
+      function (cb) {
+        return setTimeout(cb, 1000 / 60);
+      });
 
   // EXPERIMENTAL!!
   /**
@@ -37,36 +39,36 @@
    */
   function Animation(options) {
     var duration = options.duration,
-        currentIteration = 1,
-        iterations = 60 * duration,
-        start = options.start || 0,
-        end = options.end,
-        change = end - start,
-        step = options.step,
-        easing = options.easing || function easeInOutCubic(pos) {
-          // https://github.com/danro/easing-js/blob/master/easing.js
-          if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,3);
-          return 0.5 * (Math.pow((pos-2),3) + 2);
-        };
+      currentIteration = 1,
+      iterations = 60 * duration,
+      start = options.start || 0,
+      end = options.end,
+      change = end - start,
+      step = options.step,
+      easing = options.easing || function easeInOutCubic(pos) {
+        // https://github.com/danro/easing-js/blob/master/easing.js
+        if ((pos /= 0.5) < 1) return 0.5 * Math.pow(pos, 3);
+        return 0.5 * (Math.pow((pos - 2), 3) + 2);
+      };
 
     function animate() {
-      var progress = currentIteration / iterations, 
-          value = change * easing(progress) + start;
+      var progress = currentIteration / iterations,
+        value = change * easing(progress) + start;
       // console.log(progress + ", " + value);
       step(value, currentIteration);
       currentIteration += 1;
 
-      if(progress < 1) {
+      if (progress < 1) {
         requestAnimationFrame(animate);
       }
     }
+
     // start!
     requestAnimationFrame(animate);
   }
 
 
-
-  var Gauge = (function() {
+  var Gauge = (function () {
     var SVG_NS = "http://www.w3.org/2000/svg";
 
     var GaugeDefaults = {
@@ -86,14 +88,16 @@
       gaugeClass: "gauge",
       showValue: true,
       gaugeColor: null,
-      label: function(val) {return Math.round(val);}
+      label: function (val) {
+        return Math.round(val);
+      }
     };
 
     function shallowCopy(/* source, ...targets*/) {
       var target = arguments[0], sources = slice.call(arguments, 1);
-      sources.forEach(function(s) {
-        for(k in s) {
-          if(s.hasOwnProperty(k)) {
+      sources.forEach(function (s) {
+        for (k in s) {
+          if (s.hasOwnProperty(k)) {
             target[k] = s[k];
           }
         }
@@ -110,12 +114,12 @@
      */
     function svg(name, attrs, children) {
       var elem = document.createElementNS(SVG_NS, name);
-      for(var attrName in attrs) {
+      for (var attrName in attrs) {
         elem.setAttribute(attrName, attrs[attrName]);
       }
 
-      if(children) {
-        children.forEach(function(c) {
+      if (children) {
+        children.forEach(function (c) {
           elem.appendChild(c);
         });
       }
@@ -132,8 +136,8 @@
 
     function normalize(value, limit) {
       var val = Number(value);
-      if(val > limit) return limit;
-      if(val < 0) return 0;
+      if (val > limit) return limit;
+      if (val < 0) return 0;
       return val;
     }
 
@@ -162,10 +166,10 @@
     // REMEMBER!! angle=0 starts on X axis and then increases clockwise
     function getDialCoords(radius, startAngle, endAngle) {
       var cx = GaugeDefaults.centerX,
-          cy = GaugeDefaults.centerY;
+        cy = GaugeDefaults.centerY;
       return {
         end: getCartesian(cx, cy, radius, endAngle),
-      	start: getCartesian(cx, cy, radius, startAngle)
+        start: getCartesian(cx, cy, radius, startAngle)
       };
     }
 
@@ -187,24 +191,24 @@
     return function Gauge(elem, opts) {
       opts = shallowCopy({}, defaultOptions, opts);
       var gaugeContainer = elem,
-          limit = opts.max,
-          value = normalize(opts.value, limit),
-          radius = opts.dialRadius,
-          displayValue = opts.showValue,
-          startAngle = opts.dialStartAngle,
-          endAngle = opts.dialEndAngle,
-          valueDialClass = opts.valueDialClass,
-          valueTextClass = opts.valueClass,
-          valueLabelClass = opts.valueLabelClass,
-          dialClass = opts.dialClass,
-          gaugeClass = opts.gaugeClass,
-          gaugeColor = opts.color,
-          gaugeValueElem,
-          gaugeValuePath,
-          label = opts.label,
-          instance;
+        limit = opts.max,
+        value = normalize(opts.value, limit),
+        radius = opts.dialRadius,
+        displayValue = opts.showValue,
+        startAngle = opts.dialStartAngle,
+        endAngle = opts.dialEndAngle,
+        valueDialClass = opts.valueDialClass,
+        valueTextClass = opts.valueClass,
+        valueLabelClass = opts.valueLabelClass,
+        dialClass = opts.dialClass,
+        gaugeClass = opts.gaugeClass,
+        gaugeColor = opts.color,
+        gaugeValueElem,
+        gaugeValuePath,
+        label = opts.label,
+        instance;
 
-      if(startAngle < endAngle) {
+      if (startAngle < endAngle) {
         console.log("WARN! startAngle < endAngle, Swapping");
         var tmp = startAngle;
         startAngle = endAngle;
@@ -213,12 +217,12 @@
 
       function pathString(radius, startAngle, endAngle, largeArc) {
         var coords = getDialCoords(radius, startAngle, endAngle),
-            start = coords.start,
-            end = coords.end,
-            largeArcFlag = typeof(largeArc) === "undefined" ? 1 : largeArc;
+          start = coords.start,
+          end = coords.end,
+          largeArcFlag = typeof(largeArc) === "undefined" ? 1 : largeArc;
 
         return [
-          "M", start.x, start.y, 
+          "M", start.x, start.y,
           "A", radius, radius, 0, largeArcFlag, 1, end.x, end.y
         ].join(" ");
       }
@@ -245,7 +249,7 @@
 
         var angle = getAngle(100, 360 - Math.abs(startAngle - endAngle));
         var flag = angle <= 180 ? 0 : 1;
-        var gaugeElement = svg("svg", {"viewBox": "0 0 100 100", "class": gaugeClass},
+        var gaugeElement = svg("svg", { "viewBox": "0 0 100 100", "class": gaugeClass },
           [
             svg("path", {
               "class": dialClass,
@@ -263,35 +267,36 @@
 
       function updateGauge(theValue, frame) {
         var val = getValueInPercentage(theValue, limit),
-            // angle = getAngle(val, 360 - Math.abs(endAngle - startAngle)),
-            angle = getAngle(val, 360 - Math.abs(startAngle - endAngle)),
-            // this is because we are using arc greater than 180deg
-            flag = angle <= 180 ? 0 : 1;
-        if(displayValue) {
+          // angle = getAngle(val, 360 - Math.abs(endAngle - startAngle)),
+          angle = getAngle(val, 360 - Math.abs(startAngle - endAngle)),
+          // this is because we are using arc greater than 180deg
+          flag = angle <= 180 ? 0 : 1;
+        if (displayValue) {
           gaugeValueElem.textContent = label.call(opts, theValue);
         }
+
         gaugeValuePath.setAttribute("d", pathString(radius, startAngle, angle + startAngle, flag));
       }
 
       instance = {
-        setMaxValue: function(max) {
+        setMaxValue: function (max) {
           limit = max;
         },
-        setValue: function(val) {
+        setValue: function (val) {
           value = normalize(val, limit);
           updateGauge(value);
         },
-        setValueAnimated: function(val, duration) {
-        	var oldVal = value;
+        setValueAnimated: function (val, duration) {
+          var oldVal = value;
           value = normalize(val, limit);
-          if(oldVal === value) {
+          if (oldVal === value) {
             return;
           }
-          if(gaugeColor) {
-            var c = gaugeColor(value), 
-                dur = duration * 1000,
-                pathTransition = "stroke " + dur + "ms ease";
-                // textTransition = "fill " + dur + "ms ease";
+          if (gaugeColor) {
+            var c = gaugeColor(value),
+              dur = duration * 1000,
+              pathTransition = "stroke " + dur + "ms ease";
+            // textTransition = "fill " + dur + "ms ease";
 
             gaugeValuePath.style = [
               "stroke: " + c,
@@ -299,25 +304,22 @@
               "-moz-transition: " + pathTransition,
               "transition: " + pathTransition,
             ].join(";");
-            /*
+
             gaugeValueElem.style = [
               "fill: " + c,
-              "-webkit-transition: " + textTransition,
-              "-moz-transition: " + textTransition,
-              "transition: " + textTransition,
             ].join(";");
-            */
+
           }
           Animation({
             start: oldVal || 0,
             end: value,
             duration: duration || 1,
-            step: function(val, frame) {
+            step: function (val, frame) {
               updateGauge(val, frame);
             }
           });
         },
-        getValue: function() {
+        getValue: function () {
           return value;
         }
       };
